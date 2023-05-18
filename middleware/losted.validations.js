c1905adc698ce033c63ca4a70aada6d536c7556a
MohamedGabal <gabalmohamed33@gmail.com>
2023-05-19 00:08:22 +0300

    first commit

---
+++ middleware/losted.validations.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+const schema=Joi.object({
+    userId:Joi.string().required().min(6).max(30),
+    email:Joi.string().required().email(),
+    full_name:Joi.string().required(),
+    idNumber:Joi.string().required(),
+    phone:Joi.string().required(),
+    dwescribtio:Joi.string().required().min(50).max(500),
+    type:Joi.string().required(),
+    state:Joi.string().required(),
+})
+
+module.exports.lostedValidations=async (req,res,next)=>{
+    let msgArray=[]
+    let {error}=await schema.validate(req.body,{abortEarly:false})
+    if(error)
+    {
+        error.details.map((err)=>{
+            msgArray.push(err.message)
+        })
+        res.json(msgArray)
+    }else
+    {
+        next()
+    }
+}
